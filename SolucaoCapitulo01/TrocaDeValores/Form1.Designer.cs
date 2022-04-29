54d40213475587d7fb7a00bd39bb19a81c188838
Daniel Sena <danielsena.csc@gmail.com>
2022-04-29 08:19:29 -0300

    Adiciona os exemplos do capítulo um

---
+++ SolucaoCapitulo01/TrocaDeValores/Form1.Designer.cs
@@ -0,0 +1,109 @@
+﻿namespace TrocaDeValores
+{
+    partial class frmTrocaDeValores
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.txtPrimeiroValor = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.txtSegundoValor = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 16);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(43, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Valor 1:";
+            // 
+            // txtPrimeiroValor
+            // 
+            this.txtPrimeiroValor.Location = new System.Drawing.Point(61, 12);
+            this.txtPrimeiroValor.Name = "txtPrimeiroValor";
+            this.txtPrimeiroValor.Size = new System.Drawing.Size(100, 20);
+            this.txtPrimeiroValor.TabIndex = 1;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(167, 16);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(43, 13);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "Valor 2:";
+            // 
+            // txtSegundoValor
+            // 
+            this.txtSegundoValor.Location = new System.Drawing.Point(216, 12);
+            this.txtSegundoValor.Name = "txtSegundoValor";
+            this.txtSegundoValor.Size = new System.Drawing.Size(100, 20);
+            this.txtSegundoValor.TabIndex = 3;
+            // 
+            // button1
+            // 
+            this.button1.Image = global::TrocaDeValores.Properties.Resources.exchange;
+            this.button1.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.button1.Location = new System.Drawing.Point(136, 38);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(100, 23);
+            this.button1.TabIndex = 4;
+            this.button1.Text = "Trocar";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // frmTrocaDeValores
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(336, 73);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.txtSegundoValor);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.txtPrimeiroValor);
+            this.Controls.Add(this.label1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.Name = "frmTrocaDeValores";
+            this.Text = "Troca de valores";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox txtPrimeiroValor;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox txtSegundoValor;
+        private System.Windows.Forms.Button button1;
+    }
+}
+
